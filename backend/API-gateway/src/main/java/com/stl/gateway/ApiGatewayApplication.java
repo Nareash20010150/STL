0ab20352bec69dacf5a335a81693af62c215f1bd
Mr.Nareash <nareash20010150@gmail.com>
2023-09-30 22:21:33 +0530

    Implemented API-gateway for microservices

---
+++ backend/API-gateway/src/main/java/com/stl/gateway/ApiGatewayApplication.java
@@ -0,0 +1,15 @@
+package com.stl.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+@EnableEurekaClient
+public class ApiGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiGatewayApplication.class, args);
+	}
+
+}
