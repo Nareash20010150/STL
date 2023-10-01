59c68b6c48c24811033f8ab1d6d54f391a1ce715
Govindani <govindani2000@gamil.com>
2023-10-02 00:38:09 +0530

    backend

--- Bill Management/Payment/index.js
+++ Bill Management/Payment/index.js
@@ -1,20 +1,3 @@
-const express = require('express');
 
 
-const app = express();
 
-app.use(express.json());
-
-app.use('/', (req, res, next) => {
-
-    return res.status(200).json({
-        "msg": "Payment Service"
-    });
-
-});
-
-
-app.listen(4002, () => {
-
-    console.log("Payment Service is running on port 4001");
-});
